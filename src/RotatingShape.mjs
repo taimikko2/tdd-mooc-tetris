c90b108f57da7e641a8dadd329a71ef815925e22
Mikko Saarinen <el4302@gmail.com>
2023-01-19 09:43:37 +0200

    Level 2: Rotating shapes

--- src/RotatingShape.mjs
+++ src/RotatingShape.mjs
@@ -2,12 +2,8 @@ export class RotatingShape {
     shape;
 
     constructor(shape) {
-        //let a = new Array();
-        //console.log(shape);
         this.shape = this.normalize(shape);
-        //this.width = width;
-        //this.height = height;
-      }
+    }
 
     normalize(s) {
         return s.replaceAll(" ", "").trim() + "\n";
@@ -16,62 +12,31 @@ export class RotatingShape {
     rotateRight() {
         let s = "";
         let rows = this.shape.split("\n");
-        const rcount = rows.length;
-        const rlen = rows[0].length;
-        s += rows[2][0];
-        s += rows[1][0];
-        s += rows[0][0];
-        s += "\n";
-        s += rows[2][1];
-        s += rows[1][1];
-        s += rows[0][1];
-        s += "\n";
-        s += rows[2][2];
-        s += rows[1][2];
-        s += rows[0][2];
-        s += "\n";
-        
+        const rcount = rows.length - 1;
+        const len = rows[0].length;
+        for (let l = 0; l < len; l++) {
+            for (let r = rcount - 1; r >= 0; r--) {
+                s += rows[r][l];
+            }
+            s += "\n";
+        }
+        // this.shape = s;
         return s;
-        /*
-    `ABC
-     DEF
-     GHI`
-     -->
-    `GDA
-     HEB
-     IFC`
-     */
     }
 
     rotateLeft() {
-    let s = "";
+        let s = "";
         let rows = this.shape.split("\n");
-        const rcount = rows.length;
-        const rlen = rows[0].length;
-        s += rows[0][2];
-        s += rows[1][2];
-        s += rows[2][2];
-        s += "\n";
-        s += rows[0][1];
-        s += rows[1][1];
-        s += rows[2][1];
-        s += "\n";
-        s += rows[0][0];
-        s += rows[1][0];
-        s += rows[2][0];
-        s += "\n";
-        
+        const rcount = rows.length - 1;
+        const len = rows[0].length;
+        for (let l = len - 1; l >= 0; l--) {
+            for (let r = 0; r < rcount; r++) {
+                s += rows[r][l];
+            }
+            s += "\n";
+        }
+        // this.shape = s;
         return s;
-        /*
-    `ABC
-     DEF
-     GHI`
-     -->
-    `CFI
-     BEH
-     ADG`
-     */
-    
     }
 
     toString() {
