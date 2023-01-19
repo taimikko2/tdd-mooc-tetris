85f8b70497402b009813a63ec494a1e6070d494f
Mikko Saarinen <el4302@gmail.com>
2023-01-19 08:47:53 +0200

    Level 1: Falling blocks

--- src/Block.mjs
+++ src/Block.mjs
@@ -1,7 +1,22 @@
 export class Block {
   color;
+  falling;
 
   constructor(color) {
     this.color = color;
+    this.falling = true;
+  }
+
+  stopFalling() {
+    this.falling = false;
+  }
+
+  isFalling() {
+    return (this.falling === true);
+  }
+
+
+  toString() {
+    return this.color;
   }
 }
--- src/Board.mjs
+++ src/Board.mjs
@@ -1,13 +1,95 @@
 export class Board {
   width;
   height;
+  items;
+  falling;
 
   constructor(width, height) {
     this.width = width;
     this.height = height;
+    this.items = new Array(height);
+    this.falling = 0;
+    for (let i = 0; i < this.height; i++) {
+      this.items[i] = new Array(width);
+      for (let j = 0; j < this.width; j++) {
+        this.items[i][j] = ".";
+      }
+    }
+  }
+
+  drop(item) {
+    if (this.falling) {
+      throw "already falling"
+    }
+    let pos = Math.floor(this.width / 2);
+    this.items[0][pos] = item;
+    this.falling += 1;
+  }
+
+  emptyRow(rivi) {
+    this.items[rivi] = new Array(this.width);
+    for (let x in this.items[rivi]) {
+      x = ".";
+    }
+  }
+
+  rowTick(rivi) {
+    this.items[rivi] = this.items[rivi - 1];
+    this.emptyRow(rivi - 1);
+    //console.log("rowTick(" + rivi + "):\n" + this.toString());
+  }
+
+  emptyRow(rivi) {
+    let a = new Array(this.width);
+    a.fill(".");
+    this.items[rivi] = a;
+  }
+
+  stopFalling(block) {
+    if (block.isFalling()) {
+      block.stopFalling();
+      this.falling -= 1;
+    }
+  }
+
+  tick() {
+    for (let i = this.height - 1; i > 0; i--) {
+      let row = this.items[i];
+      const loc1 = row.findIndex(item => item !== ".");
+      //const loc_prev = prev_row.findIndex(item => item !== ".");
+      let curr_block = row[loc1];
+
+      //console.log(i + " rivi\n" + this.toString());
+
+      if (loc1 < 0) {
+        this.rowTick(i);
+      }
+      else {
+        this.stopFalling(curr_block);
+      }
+
+    }
+    //console.log(this.toString());
+  }
+
+  hasFalling() {
+    return (this.falling > 0);
   }
 
   toString() {
-    return "TODO";
+    let res = "";
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        const x = this.items[i][j];
+        //console.log(x);
+        if (x !== ".") {
+          res += x.toString();
+        } else {
+          res += this.items[i][j];
+        }
+      }
+      res += "\n";
+    }
+    return res;
   }
 }
